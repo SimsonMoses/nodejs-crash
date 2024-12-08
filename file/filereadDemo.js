c18e0fe2b22a4bafbf868b964ff242460536fdfe
Simson M <sims91502@gmail.com>
2024-12-08 08:55:18 +0530

    feat: create user

---
+++ file/filereadDemo.js
@@ -0,0 +1,41 @@
+import fs from 'fs'
+
+import fsp from 'fs/promises'
+
+// // callback
+// fs.readFile('./file/test.txt','utf-8',(err,data)=>{
+//     if(err) throw err;
+//     console.log(data);
+// })
+
+// // sync version
+// const data = fs.readFileSync('./file/test.txt','utf-8');
+// console.log(data)
+
+// // promise then
+// fsp.readFile('./file/test.txt','utf-8')
+//     .then((data)=>console.log(data))
+//     .catch((err)=>console.lot(err))
+
+// async await
+const readFile = async()=>{
+    try {
+        const data = await fsp.readFile('./file/test.txt','utf-8');
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const writeFile = async () => {
+    try{    
+        await fsp.writeFile('./file/test.txt','This written by js script')
+        console.log('file written');
+    }catch(err){
+        console.log(err)    
+    }
+};
+
+writeFile();
+readFile()
--- server2.js
+++ server2.js
@@ -1,3 +1,4 @@
+import { log } from 'console';
 import { createServer } from 'http'
 
 const PORT = process.env.PORT;
@@ -7,47 +8,68 @@ const users = [
     { id: 3, name: 'Simson', email: 'simson@example.com' }
 ]
 
-const logger = (req,res,next)=>{
+const logger = (req, res, next) => {
     console.log(`Method: ${req.method} Url: ${req.url}`);
-    next();    
+    next();
 }
 
 // json middleware handler
-const jsonMiddleWareHandler = (req,res,next)=>{
-    res.setHeader('Content-type','application/json')
+const jsonMiddleWareHandler = (req, res, next) => {
+    res.setHeader('Content-type', 'application/json')
     next();
 }
 
 // all user handle
-const userHandler = (res,req)=>{
+const userHandler = (res, req) => {
     res.write(JSON.stringify(users));
     res.end();
 }
-const getUserByIdHandler = (res,req)=>{
+const getUserByIdHandler = (res, req) => {
     const id = req.url.split('/')[3];
-    const user = users.find(u=>u.id===parseInt(id))
-    if(user){
+    const user = users.find(u => u.id === parseInt(id))
+    if (user) {
         res.write(JSON.stringify(user))
         res.end()
-    }else{
-        noDatahandler(res,req);
+    } else {
+        noDatahandler(res, req);
     }
 }
 // no data
-const noDatahandler = (res,req)=>{
+const noDatahandler = (res, req) => {
     res.statusCode = 404;
-    res.write(JSON.stringify({'message':'no data found'}))
+    res.write(JSON.stringify({ 'message': 'no data found' }))
     res.end()
 }
+
+// create the user
+
+const createUserHandler = (res, req) => {
+    let body = ''
+    req.on('data', (chunk) => {
+        body += chunk.toString();
+    });
+    req.on('end', () => {
+        const newUser = JSON.parse(body)
+        users.push(newUser);
+        res.statusCode = 201
+        res.write(JSON.stringify({ 'message': 'user created successfully' }))
+        res.end()
+    });
+    console.log("METHOD COMPLETED");
+    
+
+}
 const server = createServer((req, res) => {
-    logger(req,res,()=>{
-        jsonMiddleWareHandler(req,res,()=>{
-            if( req.url==='/api/users' && req.method==='GET'){
-                userHandler(res,req)
-            }else if(req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET'){
-                getUserByIdHandler(res,req)
-            }else{
-                noDatahandler(res,req)
+    logger(req, res, () => {
+        jsonMiddleWareHandler(req, res, () => {
+            if (req.url === '/api/users' && req.method === 'GET') {
+                userHandler(res, req)
+            } else if (req.url.match(/\/api\/users\/([0-9]+)/) && req.method === 'GET') {
+                getUserByIdHandler(res, req)
+            } else if (req.url === '/api/users' && req.method === 'POST') {
+                createUserHandler(req, res)
+            } else {
+                noDatahandler(res, req)
             }
         })
     })
