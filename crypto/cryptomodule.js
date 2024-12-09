c54fa1ecbfeb32ad474e9e1423b75e137f7d6faa
Simson M <sims91502@gmail.com>
2024-12-09 19:31:03 +0530

    empty

--- crypto/cryptomodule.js
+++ crypto/cryptomodule.js
@@ -24,6 +24,6 @@ encrypted += cipher.final('hex')
 console.log(encrypted);
 
 const decipher = crypto.createDecipheriv(algorithm, key, iv)
-let decrypted = decipher.update(encrypted,'hex','utf-8')
-decrypted += decipher.final('utf-8')
+let decrypted = decipher.update(encrypted,'hex','utf8')
+decrypted += decipher.final('utf8')
 console.log(decrypted);
